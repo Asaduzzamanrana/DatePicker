938f4701d7a6bdf0253fb54679ee2f091e7c7c5b
Asaduzzamanrana <cseseu100@gmail.com>
2021-01-16 01:20:42 +0600

    DatePicker

---
+++ app/src/main/java/com/example/datepicker/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.datepicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Button button;
+    private DatePicker datePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView=findViewById(R.id.datetextviewid);
+        button=findViewById(R.id.buttonid);
+        datePicker=findViewById(R.id.datepickerid);
+        textView.setText(currentDate());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText(currentDate());
+            }
+        });
+
+    }
+    String currentDate(){
+        StringBuilder stringBuilder=new StringBuilder();
+
+        stringBuilder.append("Current Date: ");
+        stringBuilder.append(datePicker.getDayOfMonth()+"/");
+        stringBuilder.append((datePicker.getMonth()+1)+"/");
+        stringBuilder.append(datePicker.getYear());
+        return stringBuilder.toString();
+    }
+}
